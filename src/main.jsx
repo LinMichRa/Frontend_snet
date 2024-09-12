2ea399b6120389f414fa92ad189121eaf823095d
LinMichRa <lramirezd2@ucentral.edu.co>
2024-09-12 12:44:02 -0500

    css's files implemented and main fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './assets/css/normalize.css';
+import './assets/css/responsive.css';
+import './assets/css/styles.css';
+import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css';
 
 createRoot(document.getElementById('root')).render(
     <App />
-)
+) 
